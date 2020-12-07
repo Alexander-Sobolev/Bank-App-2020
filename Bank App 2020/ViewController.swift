efa7eaf4fc8d89c52c6934c11c9042beb0584fc1
Alexander Sobolev <sobolev.ios.developer@gmail.com>
2020-12-07 18:47:09 +0300

    Initial Commit

---
+++ Bank App 2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bank App 2020
+//
+//  Created by Alexander Sobolev on 07.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
